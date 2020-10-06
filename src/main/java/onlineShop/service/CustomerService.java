90210f22c21afab8821eada6058d8bff6001612a
Jinfeng Chen <jinfeng.chen@sjsu.edu>
2020-10-06 15:16:12 -0700

    First commit

---
+++ src/main/java/onlineShop/service/CustomerService.java
@@ -0,0 +1,26 @@
+package onlineShop.service;
+
+import onlineShop.dao.CustomerDao;
+import onlineShop.model.Cart;
+import onlineShop.model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+    @Autowired
+    private CustomerDao customerDao;
+
+    public void addCustomer(Customer customer) {
+        Cart cart = new Cart();
+        customer.setCart(cart);
+        customer.getUser().setEnabled(true);
+        customerDao.addCustomer(customer);
+
+
+    }
+
+    public Customer getCustomerByEmail(String email) {
+        return customerDao.getCustomerByEmail(email);
+    }
+}
